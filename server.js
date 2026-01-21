31d45e23b63fa9468255836720da1a10dc4161ae
EquaNova-org <maxmudov.ozodbek77@gmail.com>
2026-01-21 21:58:52 +0300

    server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/programs", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "programs.html"));
+  res.sendFile(path.join(__dirname, "public", "events.html"));
 });
 
 /* =====================
@@ -45,5 +45,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
