7469ba41e3bead6da4295829f19b9ef0be92001a
EquaNova-org <maxmudov.ozodbek77@gmail.com>
2026-03-20 19:43:07 +0300

    Delete src/bookingRoutes.js

--- src/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createBookingIntent } = require("../controllers/bookingController");
-
-// later: auth middleware plugs here
-router.post("/book/:opportunityId", createBookingIntent);
-
-module.exports = router;
